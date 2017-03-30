ec1fc35ef7913d4975eb54c18e9c59dee43a85e3
Byron_Olate <byron.olate@gmail.com>
2017-03-30 10:40:34 -0200

    Banner tipo Tv News

---
+++ js/clima.js
@@ -0,0 +1,13 @@
+conte = document.getElementById("cont_708e5aa9b27b83f5d6bf908bb79fff23");
+                        if (conte) {
+                            conte.style.cssText = "position:relative;left:8px; width: 111px; color: transparent; background-color:transparent; border:1px solid transparent; margin: 0 auto; font-family: Roboto;";
+                            elem = document.createElement("iframe");
+                            elem.style.cssText = "width:111px; color:transparent; height:155px;";
+                            elem.id = "708e5aa9b27b83f5d6bf908bb79fff23";
+                            elem.src = "https://www.tiempo.com/getwid/708e5aa9b27b83f5d6bf908bb79fff23";
+                            elem.frameBorder = 0;
+                            elem.allowTransparency = true;
+                            elem.scrolling = "no";
+                            elem.name = "flipe";
+                            conte.appendChild(elem);
+                        }
